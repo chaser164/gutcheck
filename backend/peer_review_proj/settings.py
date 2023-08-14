56d5fc4f8c5b4cd3cb0b2c69203a71274a9c5ab0
Chase Reynders <chase.reynders@yale.edu>
2023-08-14 17:39:10 -0400

    signup password confirmation

--- backend/peer_review_proj/settings.py
+++ backend/peer_review_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)je--=2$&=dy%_x(zq3p-)c1-1r@_vbdi5fnbaycyhs%1lzba1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'http://localhost:5173']
 
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'http://localhost:5173']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
