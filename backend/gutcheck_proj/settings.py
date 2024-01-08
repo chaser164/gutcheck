cb551771704ef1cf4661a24d6f3c1d38fa2aa881
Chase Reynders <chase.reynders@yale.edu>
2024-01-08 13:36:31 -0500

    fixed permissiions

--- backend/gutcheck_proj/settings.py
+++ backend/gutcheck_proj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'gutcheck-chaser164.pythonanywhere']
+ALLOWED_HOSTS = ['127.0.0.1', 'gutcheck-chaser164.pythonanywhere.com']
 
 
 # Application definition
