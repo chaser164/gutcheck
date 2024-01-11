a8a8e31408033f5a622951565e579c24d60c8964
Chase Reynders <chase.reynders@yale.edu>
2024-01-11 18:09:54 -0500

    added privacy policy link

--- frontend/email-conf-pass-reset-frontend/src/components/HomePage.jsx
+++ frontend/email-conf-pass-reset-frontend/src/components/HomePage.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
 
   return (
     <>
-      <h3>Welcome To GutCheck!</h3>
+      <h3>The human-run misinformation reporter</h3>
+      <br />
+      <Link to="/privacy">Privacy Policy</Link>
     </>
   );
 }
